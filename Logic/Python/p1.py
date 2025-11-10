e6b37603fbdade43f80c5c1a0db78f89f6818596
Shivaraj Hiremath <shivaraj94826@gmail.com>
2025-11-10 22:58:45 +0530

    Day 15

--- Logic/Python/p1.py
+++ Logic/Python/p1.py
@@ -654,3 +654,32 @@ print("Extracted min:", min_heap.extract_min())
 print("Extracted min:", min_heap.extract_min())
 
 # =======================
+# 2️⃣8️⃣ RADIX SORT
+# =======================
+
+def counting_sort_for_radix(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+    for i in range(n):
+        index = (arr[i] // exp) % 10
+        count[index] += 1
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    for i in range(n - 1, -1, -1):
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+    for i in range(n):
+        arr[i] = output[i]
+def radix_sort(arr):
+    max1 = max(arr)
+    exp = 1
+    while max1 // exp > 0:
+        counting_sort_for_radix(arr, exp)
+        exp *= 10
+arr_radix = [170, 45, 75, 90, 802, 24, 2, 66]
+radix_sort(arr_radix)
+print("Radix sorted array:", arr_radix)
+
+# =======================
